271645bda28635c4ec87e740b819fa7f88eac3ed
Onyedikachi <kachigirl54@gmail.com>
2026-08-11 22:31:03 +0100

    Refactor index.js for improved structure and add package.json for project configuration

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import path from "path";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
 import rateLimiter from "../src/middleware/rateLimiter.js"
@@ -9,17 +11,27 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve()
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-  }),
-);
+if(process.env.NODE_ENV !== "production"){
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+    }),
+  );
+}
 app.use(express.json())
 app.use(rateLimiter)
 
 app.use("/api/notes", notesRoutes)
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+  app.get("*", (req, res)=>{
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 connectDB().then(() => {
   app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
