fe48ef836cf0b08d4e0909062b6f379d08ccc221
Onyedikachi <kachigirl54@gmail.com>
2026-08-11 22:57:50 +0100

    Fix production frontend routing

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,38 +2,39 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-
 import notesRoutes from "./routes/notesRoutes.js";
-import {connectDB} from "./config/db.js";
-import rateLimiter from "../src/middleware/rateLimiter.js"
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
       origin: process.env.FRONTEND_URL,
     }),
   );
 }
-app.use(express.json())
-app.use(rateLimiter)
 
-app.use("/api/notes", notesRoutes)
+app.use(express.json());
+app.use(rateLimiter);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.use("/api/notes", notesRoutes);
 
-  app.get("*", (req, res)=>{
-  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+  });
 }
+
 connectDB().then(() => {
   app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`);
+  });
 });
